3f7d398e8da66eebdc04805fa826d4e9080f1967
Alexandre Guz <alexandreguz@Alexandres-MacBook-Pro.local>
2020-04-30 16:42:41 +0300

    fix for 400 colums

--- lib/retorno_generetor.exs
+++ lib/retorno_generetor.exs
@@ -9,7 +9,7 @@
       dac_nosso_numero = String.slice(remessa, 28..28)
       blanks_3 = String.duplicate(" ", 13)
       codigo_da_carteira = "I"
-      codigo_de_ocorrencia_variavel = "02" # 02 - entrada, 03 - rejeitado, 06 - liquidado, 14 - vencimento alterado,
+      codigo_de_ocorrencia_variavel = "06" # 02 - entrada, 03 - rejeitado, 04 - alteracao de dados, 06 - liquidado, 09 - baixa simples, 14 - vencimento alterado,
       today = Date.utc_today
       day = today.day |> Integer.to_string() |> String.pad_leading(2, "0")
       month = today.month |> Integer.to_string() |> String.pad_leading(2, "0")
@@ -18,22 +18,27 @@
       numero_do_documento = String.slice(remessa, 110..119)
       nosso_numero = String.slice(remessa, 62..69)
       blanks_4 = String.duplicate(" ", 12)
-      due_date = String.slice(remessa, 121..126)
+      due_date = String.slice(remessa, 120..125)
       value = String.slice(remessa, 126..138)
       bank_code = "001"
       branch = "3421"
       dac_branch = "5"
-      kind = "05"
+      kind = "05" #most
       zeros_1 = String.duplicate("0", 78)
       main_value = String.slice(remessa, 126..138)
       zeros_2 = String.duplicate("0", 26)
       blanks_5 = String.duplicate(" ", 3)
-      payment_day = "DDMMYY"
+      #payment_day = "DDMMYY" # if codigo_ocorrencia_variavel = 06
+      today = Date.utc_today
+      day = today.day |> Integer.to_string() |> String.pad_leading(2, "0")
+      month = today.month |> Integer.to_string() |> String.pad_leading(2, "0")
+      year = today.year |> Integer.to_string() |> String.slice(2..3)
+      payment_day = day <> month <> year
       zeros_3 = String.duplicate("0", 23)
       sacado_name = String.slice(remessa, 234..263)
       blanks_6 = String.duplicate(" ", 38)
       liquidation_code = "B3" # This can vary but is not important
-      senquencial_number = (index  + 1) |> Integer.to_string() |> String.pad_leading(5, "0")
+      senquencial_number = (index  + 1) |> Integer.to_string() |> String.pad_leading(6, "0")
 
 
 
